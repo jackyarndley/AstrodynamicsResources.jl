d147123c8ad29321d14d90e4b28dc15cfde52fb0
Jack Yarndley <34801340+jackyarndley@users.noreply.github.com>
2026-08-17 15:31:33 +1200

    Run planet texture tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ run_group(group::AbstractString) = GROUP in ("All", group)
 if run_group("Core")
     @time @safetestset "catalogue" include("catalog.jl")
     @time @safetestset "catalogue validation" include("catalog_validation.jl")
+    @time @safetestset "planet textures" include("textures.jl")
     @time @safetestset "artifacts" include("artifacts.jl")
     @time @safetestset "scratch" include("scratch.jl")
 end
